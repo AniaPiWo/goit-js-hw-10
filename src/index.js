4ad444bac2d560ac50a99f2278359056a2948d68
AniaPiWo <snowflake@wp.pl>
2023-12-14 11:35:18 +0100

    feat: loader, error msg

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import { fetchBreeds, fetchCatByBreed } from './api';
 
 const select = document.querySelector('select');
 const catCard = document.querySelector('.cat-card');
+const loader = document.querySelector('.loader');
+const error = document.querySelector('.error');
 
 select.addEventListener('change', createCatCard);
 updateSelect();
+
+loader.classList.add('hidden');
 
 function updateSelect(data) {
   fetchBreeds(data)
@@ -14,28 +18,40 @@ function updateSelect(data) {
       });
       select.insertAdjacentHTML('beforeend', markSelect);
     })
-    .catch(onFetchError);
+    .catch(error => {
+      loader.classList.add('hidden');
+      error.classList.remove('hidden');
+      console.error('Error fetching breeds:', error);
+    });
 }
 
 function createCatCard(event) {
   const breedId = event.currentTarget.value;
 
+  loader.classList.remove('hidden');
+
   fetchCatByBreed(breedId)
     .then(data => {
       const { url, breeds } = data[0];
-
+      loader.classList.add('hidden');
+      //console.log(breeds);
       catCard.innerHTML = `<div class="catCard">
       <img src="${url}" alt="${breeds[0].name}" width="400" />
       <div class="descr">
-        <h2>${breeds[0].name}</h2>
-        <p>${breeds[0].description}</p>
-        <p><strong>Temperament:</strong> ${breeds[0].temperament}</p>
-      </div>
+        <h2 class="characteristic">${breeds[0].name}</h2>
+        <h3>${breeds[0].description}</h3>
+        <div class="char_box">
+          <p><span class="characteristic">Origin:</span> ${breeds[0].origin}</p>
+          <p><span class="characteristic">Life span:</span> ${breeds[0].life_span}</p>
+          <p><span class="characteristic">Weight:</span> ${breeds[0].weight}</p>
+          <p><span class="characteristic">Temperament:</span> ${breeds[0].temperament}</p>
+          </div>
+        </div>
     </div>`;
     })
-    .catch(onFetchError);
-}
-
-function onFetchError() {
-  console.log('Error fetching breeds');
+    .catch(error => {
+      loader.classList.add('hidden');
+      error.classList.remove('hidden');
+      console.error('Error fetching cat by breed:', error);
+    });
 }
